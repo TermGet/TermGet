46e5a155e32621a202793054d9b38e80e36d8193
PizzaLovingNerd <camck05@gmail.com>
2018-02-10 20:15:36 -0800

    Fixed another yaourt issue

--- termget.py
+++ termget.py
@@ -90,7 +90,7 @@ while True: #Starts a loop
             if user1 == "1":
                 os.system("sudo pacman -Ss " + user)
             if user1 == "2":
-                os.system("sudo yaourt -Ss " + user)
+                os.system("yaourt -Ss " + user)
         elif package == "xbps":
             os.system("sudo xbps-query -Rs " + user)
         elif package == "dnf":
