a09b4ef9ad83d59d7f4e8b605f998667d0a76b68
PizzaLovingNerd <pizzalovingnerd@teknik.io>
2018-06-06 23:50:06 +0000

    Untitled

---
+++ install-solus.sh
@@ -0,0 +1,72 @@
+#!/bin/bash
+
+# autodetection. If the incorrect one shows, it is possible that you have a program on your system that has the same name as a package manager.
+if [ "$1" != "--no-detection" ]; then
+	if [[ $(which brew 2> /dev/null) ]]; then
+		echo -n "Why do you torture yourself with using Mac OS? Fine, homebrew will be used. Is this correct? [y/n] "
+		pm=homebrew
+	elif [[ $(which xbps-install 2> /dev/null) ]]; then
+		echo -n "A Void or Void-based system has been detected. XBPS will be used. Is this correct? [y/n] "
+		pm=xbps
+	elif [[ $(which dnf 2> /dev/null) ]]; then
+		echo -n "A Fedora or Fedora-based system has been detected. DNF will be used. Is this correct? [y/n] "
+		pm=dnf
+	elif [[ $(which zypper 2> /dev/null) ]]; then
+		echo -n "An openSUSE or openSUSE-based system has been detected. Zypper will be used. Is this correct? [y/n] "
+		pm=zypper
+	elif [[ $(which eopkg 2> /dev/null) ]]; then
+		echo -n "A Solus or Solus-based system has been detected. Eopkg will be used. Is this correct? [y/n] "
+		pm=eopkg
+	elif [[ $(which crew 2> /dev/null) ]]; then
+		echo -n "Chromebrew (an unoffical Chrome/Chromium OS package manager) has been detected. Is this correct? [y/n] "
+		pm=chromebrew
+	elif [[ $(which emerge 2> /dev/null) ]]; then
+		echo -n "A Gentoo or Gentoo-based system has been detected. Emerge will be used. Is this correct? [y/n] "
+		pm=emerge
+		echo "Good thing you don't have to compile this"
+	elif [[ $(which pkg 2> /dev/null) ]]; then
+		echo -n "You're one of those weird FreeBSD users, aren't you? If so, pkg will be used. Is this correct? [y/n] "
+		pm=pkg
+	elif [[ $(which pacman 2> /dev/null) ]]; then
+		echo -n "An Arch or Arch-based system has been detected. Pacman will be used. Is this correct? [y/n] "
+		pm=pacman
+	elif [[ $(which apt-get 2> /dev/null) ]]; then
+		echo -n "A Debian or Debian-based system has been detected. Apt-get will be used. Is this correct? [y/n] "
+		pm=apt-get
+	elif [[ $(which nix 2> /dev/null) ]]; then
+		echo -n "A NixOS or NixOS-based system has been detected. Nix will be used. Is this correct? [y/n] "
+		pm=nix
+	else
+		echo -n "A package manager has failed to be detected. If you proceed to install the program, termget will ask 
+	you to set a package manager manually on first launch. Proceed to install? [y/n] "
+	fi
+
+	read answer
+	if [ $answer != "y" ] && [ -z $pm ]; then
+		echo "Installation aborted."
+		exit
+	elif [ $answer != "y" ] && [ -n $pm ]; then
+		echo -n "A package manager has failed to be detected. If you proceed to install the program, termget will ask 
+	you to set a package manager manually on first launch. Proceed to install? [y/n] "
+		read answer2
+		if [ $answer2 != "y" ]; then
+			echo "Installation aborted."
+			exit
+		else
+			pm=
+		fi
+	fi
+fi
+
+echo "setting up directories"
+sudo bash -c "mkdir /usr/share/termget 2> /dev/null" # create new config directory if one doesn't exist
+echo "... installing program to /usr/local/bin"
+chmod +x termget.py
+sudo cp termget.py /usr/bin/termget # copy program to PATH
+
+if [ "$1" != "--no-detection" ]; then
+	echo "... generating package file"
+	sudo bash -c "echo -n $pm > /usr/share/termget/termget-package-manager" # copy package file to config directory
+fi
+
+echo -e "\nSuccessfully Installed!"
