0459cb655b74e7112e8c710bda0cc3be69492018
PizzaLovingNerd <pizzalovingnerd@teknik.io>
2018-06-07 01:40:51 +0000

    slurp slurp

--- install-solus.sh
+++ install-solus.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-# autodetection. If the incorrect one shows, it is possible that you have a program on your system that has the same name as a package manager.
-if [ "$1" != "--no-detection" ]; then
-    pm=eopkg
+pm=eopkg
 
 echo "setting up directories"
 sudo bash -c "mkdir /usr/share/termget 2> /dev/null" # create new config directory if one doesn't exist
